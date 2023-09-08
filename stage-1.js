1644518fafb6603d81fbdc333498452898506e52
AZEEZ VICTOR SEGUN <92987883+azeezz0090@users.noreply.github.com>
2023-09-08 19:09:19 +0100

    Update stage-1.js

--- stage-1.js
+++ stage-1.js
@@ -1,12 +1,34 @@
-function currentTime() {
-    const dayOfWeek = new Date().toLocaleDateString('en-US',{
-        weekday: 'long'
-    })
-
-    const utcTime = new Date().toLocaleTimeString();
-
-    document.querySelector('[data-testid = "currentDayOfThWeek"]').textContent = dayOfWeek;
-    document.querySelector('[data-testid="currentTime"]').textContent = utcTime;
+// Function to update the current day of the week
+function updateCurrentDayOfTheWeek() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDate = new Date();
+  const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+  document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
+    dayOfWeek;
 }
 
-setInterval(currentTime, 1000);
+// Function to update the current UTC time
+
+function updateCurrentUTCTime() {
+  const currentMillis = new Date().getTime();
+  document.querySelector('[data-testid="currentUTCTime"]').textContent =
+    currentMillis;
+}
+
+// Initial update of real-time data
+updateCurrentDayOfTheWeek();
+updateCurrentUTCTime();
+
+// Update real-time data every second
+setInterval(function () {
+  updateCurrentDayOfTheWeek();
+  updateCurrentUTCTime();
+}, 1000)
